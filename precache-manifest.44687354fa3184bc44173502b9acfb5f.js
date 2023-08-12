0f2ef6f2a7cd8a05d7de667834af0fe9de3224cc
rohitwebdeveloper <you@example.com>
2023-08-12 12:24:12 +0530

    Updates

---
+++ precache-manifest.44687354fa3184bc44173502b9acfb5f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a18a51a1f346f6722ae3",
+    "url": "/react-weather-app/static/css/main.62ec64e8.chunk.css"
+  },
+  {
+    "revision": "a18a51a1f346f6722ae3",
+    "url": "/react-weather-app/static/js/main.a18a51a1.chunk.js"
+  },
+  {
+    "revision": "902636a24394931e1ffa",
+    "url": "/react-weather-app/static/js/1.902636a2.chunk.js"
+  },
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "62331cf8c9394b59c9d4dcae8ad9ed3c",
+    "url": "/react-weather-app/static/media/location.62331cf8.png"
+  },
+  {
+    "revision": "4f833284dacac37579eeef23cf147a6f",
+    "url": "/react-weather-app/static/media/rain.4f833284.png"
+  },
+  {
+    "revision": "d3793a2c28f1d1a59824aa8def1f757f",
+    "url": "/react-weather-app/static/media/clear.d3793a2c.png"
+  },
+  {
+    "revision": "d26a82b89cb2af0018c67474171fc8c6",
+    "url": "/react-weather-app/static/media/drizzle.d26a82b8.png"
+  },
+  {
+    "revision": "8d5deced27dcef26fd26bbdc5931765b",
+    "url": "/react-weather-app/static/media/thunderstrom.8d5deced.png"
+  },
+  {
+    "revision": "c0f4acc14e8fd43a769048c953e9e46f",
+    "url": "/react-weather-app/index.html"
+  }
+];
